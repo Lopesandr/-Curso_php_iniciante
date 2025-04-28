079a25a4faf11c4bcb27759795514f816995d139
Andressa Lopes <115747071+Lopesandr@users.noreply.github.com>
2025-04-28 15:22:21 -0300

    Add files via upload
    Adicionando a seção 2

---
+++ exercicio2.php
@@ -0,0 +1,11 @@
+<?php
+$nome = "Andressa"; // Essa variavél contem o nome de uma pessoa 
+
+$idade = 23; #  Essa variavél contem a idade de uma pessoa  
+
+/*
+   $cidade =  em Porto Alegre;
+   */
+
+echo $nome;
+echo $idade;
