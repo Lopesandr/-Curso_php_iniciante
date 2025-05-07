c39fc54eb0b7226cfe166c1388af8769c198f635
Andressa Lopes <115747071+Lopesandr@users.noreply.github.com>
2025-05-07 15:07:48 -0300

    Add files via upload
    Adicionando exercício 6 e 7.

---
+++ secao_03/exercicio6.php
@@ -0,0 +1,9 @@
+<?php
+
+$carro = ['Modelo' => 'Palio', 'Marca' => 'Fiat', 'Ano' => 2013];
+
+echo $carro['Modelo'];
+
+echo $carro['Marca'];
+
+echo $carro['Ano'];
---
+++ secao_03/exercicio7.php
@@ -0,0 +1,10 @@
+<?php
+
+$pessoa = ['nome' => 'Andressa', 'idade' => 18];
+
+if ($pessoa['idade'] > 18) {
+
+    echo 'É de maior';
+} else {
+    echo 'É de menor';
+}
