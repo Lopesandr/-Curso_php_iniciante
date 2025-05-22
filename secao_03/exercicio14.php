ebb818344f0da2102be24d69189bc98d79446927
Andressa Lopes <115747071+Lopesandr@users.noreply.github.com>
2025-05-22 17:20:14 -0300

    Add files via upload
    Adicionando novos exercícios da seção 5.

---
+++ secao_03/exercicio14.php
@@ -0,0 +1,5 @@
+<?php
+
+$saudacao = 'Dr.';
+
+echo $saudacao . " Luiz";
---
+++ secao_03/exercicio15.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 3;
+$b = 3;
+$c = 5;
+
+if ($a == $b) {
+    echo True;
+}
+
+
+if ($a == $c) {
+    echo True;
+}
