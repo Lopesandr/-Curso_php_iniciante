c2b49d6c40151e80c90033124e898287799d977a
Andressa Lopes <115747071+Lopesandr@users.noreply.github.com>
2025-06-03 16:09:21 -0300

    Add files via upload
    Adicionando mais exercícios da seção 5.

---
+++ secao_05/exercicio17.php
@@ -0,0 +1,14 @@
+<?php
+
+
+if (15 > 5 && "João" === "João") {
+    echo "A comparação 1 é verdadeira";
+}
+
+if ("teste" > 5 && 1) {
+    echo "A comparação 2 é verdadeira";
+}
+
+if (2 == 3 && 5 >= 3) {
+    echo "A comparação 3 é verdadeira";
+}
---
+++ secao_05/exercicio20.php
@@ -0,0 +1,6 @@
+<?php
+
+$a = 1;
+$b = 2;
+
+echo $a <= $b ? "$a é menor que $b" :  "$a é maior que $b";
